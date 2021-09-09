94995217cb70784a494d4b275eab44a41302a8a3
bobyang1234 <qyang2462@gmail.com>
2021-09-09 22:16:19 +1000

    Added Robot class and Robot.CheckValidInput method and tested

---
+++ Robot Challenge/Robot Challenge/Robot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Robot_Challenge
+{
+    public class Robot
+    {
+        public int current_x_position { get; set; }
+        public int current_y_position { get; set; }
+        public string current_orientation { get; set; }
+
+        public bool CheckValidInput(string input)
+        {
+            Regex rgx = new Regex("^((MOVE)|(REPORT)|(LEFT)|(RIGHT)|(PLACE [0-9]+,[0-9]+,(north|south|east|west)))$", RegexOptions.IgnoreCase);
+            if (rgx.IsMatch(input))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Robot Challenge/RobotTests/RobotTests.cs
@@ -0,0 +1,55 @@
+﻿using Robot_Challenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RobotTests
+{
+    public class RobotTests
+    {
+        Robot robot = new Robot();
+
+        [Theory]
+        [InlineData("PLACE 0,0,NORTH")]
+        [InlineData("place 0,0,NORTH")]
+        [InlineData("PLACE 0,0,north")]
+        [InlineData("PLACE 0,0,EAST")]
+        [InlineData("PLACE 0,0,east")]
+        [InlineData("PLACE 0,0,WEST")]
+        [InlineData("PLACE 0,0,west")]
+        [InlineData("PLACE 0,0,SOUTH")]
+        [InlineData("PLACE 0,0,south")]
+        [InlineData("PLacE 0,0,noRTh")]
+        [InlineData("PLacE 23420,53252360,noRTh")]
+        [InlineData("PLacE 4,4,noRTh")]
+        [InlineData("Left")]
+        [InlineData("Right")]
+        [InlineData("MoVe")]
+        [InlineData("REpORt")]
+        public void CheckValidInputShouldBeTrue(string input)
+        {
+            Assert.True(robot.CheckValidInput(input));
+        }
+
+        [Theory]
+        [InlineData("PLACE  0,0,NORTH")]
+        [InlineData(" PLACE 0,0,NORTH")]
+        [InlineData("PLACE  0,0,NORTH ")]
+        [InlineData("PLACE -10,20,NORTH")]
+        [InlineData("PLACE 10,-2,NORTH")]
+        [InlineData("PLACE 1.23,2,NORTH")]
+        [InlineData("PLACE 1,2.345,NORTH")]
+        [InlineData("leeft")]
+        [InlineData(" left ")]
+        [InlineData(" right ")]
+        [InlineData(" move ")]
+        [InlineData(" report ")]
+        public void CheckValidInputShouldBeFalse(string input)
+        {
+            Assert.False(robot.CheckValidInput(input));
+        }
+    }
+}
