44d900b807038fbddab4e9fcf2a6d492453f862d
bobyang1234 <qyang2462@gmail.com>
2021-09-09 22:11:37 +1000

    Initial form design and read input button code

--- Robot Challenge/Robot Challenge/Form1.cs
+++ Robot Challenge/Robot Challenge/Form1.cs
@@ -9,12 +9,28 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Robot_Challenge
-{
+{    
     public partial class Form1 : Form
     {
+        private List<string> commands;
         public Form1()
         {
             InitializeComponent();
+            commands = new List<string>();
+        }
+
+        private void btn_readinputs_Click(object sender, EventArgs e)
+        {
+            commands.Clear();
+            for (int i = 0; i < txtbox_input.Lines.Length; i++)
+            {
+                commands.Add(txtbox_input.Lines[i]);
+            }
+        }
+
+        private void btn_readfromfile_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
